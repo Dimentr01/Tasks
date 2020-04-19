1be8a0e6b7dd9e506393d40b41cee9f6655803ab
Dimentr <dima.zaharcev@mail.ru>
2020-04-19 18:43:27 +0300

    Find element by id (not for number)
    Add lines: delete A...

--- Task4/Task4/Source.cpp
+++ Task4/Task4/Source.cpp
@@ -40,61 +40,81 @@ bool Equal(string a, string b) {
 void insert_after_given_element(list**A)
 {
     int x;
-    cout << "Insert the number of the element before which we insert the number\n";
+    cout << "Insert the id of the element before which we insert the number\n";
     cin >> x;
     list* B = new list();
     list* C = new list();
+    B = (*A)->next;
     C = *A;
-    B = *A;
-    for (int i = 0; i < x; i++)
+    int t = 1;
+    while (C != NULL)
     {
-        B = C;
+        if (C->id == x)
+        {
+            cout << "Enter your element\n";
+            int t;
+            cin >> t;
+            list* F = new list();
+            F->id = t;
+            C->next = F;
+            F->next = B;
+            break;
+        }
         C = C->next;
+        B = B->next;
     }
-    cout << "Enter your element\n";
-    int t;
-    cin >> t;
-    list* F = new list();
-    F->id = t;
-    B->next = F;
-    F->next = C;
-}
-
-void insert_before_given_element(list** A) {
-    int x;
-    cout << "Insert the number of the element before which we insert the number\n";
-    cin >> x;
-    list* B = new list();
-    list* C = new list();
-    C = *A;
-    B = *A;
-    for (int i = 0; i < x-1; i++)
-    {
-        B = C;
-    	C = C->next;
-    }
-    cout << "Enter your element\n";
-    int t;
-	cin >> t;
-    list* F = new list();
-    F->id = t;
-    B->next = F;
-    F->next = C;
 }
 
 void push_element_in_begin(list** A) {
-    int x; 
+    int x;
     cout << "Enter element, that you want\n";
     cin >> x;
-	list* B = new list();
+    list* B = new list();
     B->id = x;
     B->next = *A;
     *A = B;
 }
 
+
+void insert_before_given_element(list** A) {
+    int x;
+    cout << "Insert the id of the element before which we insert the number\n";
+    cin >> x;
+    list* B = new list();
+    list* C = new list();
+    C = *A;
+    B = (*A)->next;
+    int l = 0;
+    while (B != NULL)
+    {
+      if (l==0 && C->id==x)
+        {
+      	  push_element_in_begin(A);
+          break;
+        }
+        if (B->id == x)
+            {
+                cout << "Enter your element\n";
+                int t;
+                cin >> t;
+                list* F = new list();
+                F->id = t;
+                C->next = F;
+                F->next = B;
+                break;
+            }
+        l++;
+        C = C->next;
+        B = B->next;
+    }
+}
+
 void writeln(list* A) {
     if (A!= NULL) {
-        cout << A->id << endl;
+        if (A->next != NULL)
+            cout << A->id << "->";
+        else
+            cout << A->id <<endl;
         writeln(A->next);
     }
 }
@@ -123,20 +143,28 @@ void delete_last_element(list** A)
 
 void delete_given_element(list** A)
 {
+    int x;
+    cout << "Write the id of element, which we delete\n";
+    cin>>x;
     list* B = new list();
-    B = *A;
-    int t;
-    cout << "Write the number of element, which we delete\n";
-    cin>>t;
-    for(int i=0;i<t-2;i++)
+    list* C = new list();
+    B = (*A)->next;
+    C = *A;
+    int p = 1;
+    while (B != NULL)
     {
+        if (p==1 && C->id==x)
+        {
+            delete_first_element(A);
+            break;
+        }
+        if (B->id == x)
+        {
+            C->next = C->next->next;
+        }
+        C = C->next;
         B = B->next;
-    }
-    if (t == 1)
-        delete_first_element(A);
-    else
-    {
-        B->next = B->next->next;
+        p++;
     }
 }
 
@@ -156,7 +184,7 @@ void push_list_in_begin(list** A) {
         B = C;
     }
     writeln(B);
-    cout << "If you want to insert this list in A's begin write Yes\n";
+    cout << "\nIf you want to insert this list in A's begin write Yes\n";
     string a;
     cin >> a;
     D = B;
@@ -219,7 +247,7 @@ int main() {
 	string a;
     cout << "Choose A.number what you want:\n" << "B task is individual\n" << "A1 push element in begin\n" << "A2 push list in begin\n" << "A3 show all elements\n";
     cout << "A4 insert before given element\n" << "A5 insert after given element\n" << "A6 insert last element\n" << "A7 delete first element\n" << "A8 delete last element\n";
-    cout <<"A9 delete given element\n"<<"B4 delete all minus elements\n";
+    cout <<"A9 delete given element\n"<<"B4 delete all minus elements\n"<<"\nIf you want end, write Exit\n";
     for (int i = 0; i < 20; i++)
     {
         cin >> a;
@@ -273,5 +301,10 @@ int main() {
              delete_all_minus_elements(&A);
              cout << "Complete\n";
          }
+         if (Equal(a, "Exit") == 1)
+         {
+             break;
+         }
     }
+    delete A;
 }
